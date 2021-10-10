3f9b570398237c6f938016201f594b0a86c12f44
unknown <mimo.aref@gmail.com>
2021-10-10 22:24:22 +0100

    menu done

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,12 @@
+import './homepage.styles.scss';
+import Directory from "../../components/directory/directory.component";
+
+const HomePage = () => (
+    <div className='homepage'>
+        <Directory>
+
+        </Directory>
+
+    </div>
+)
+export default HomePage;
