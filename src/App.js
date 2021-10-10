30f3e36a29b10d4d6e5729363825e9909299cc6e
unknown <mimo.aref@gmail.com>
2021-10-10 19:40:35 +0100

    first step

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import HomePage from './hoepage.component';
+
+function App() {
+    return (
+        <div>
+            <HomePage></HomePage>
+        </div>
+    );
+}
+
+export default App;
